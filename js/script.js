c2a5c5d9a992ac553e7e9313e7446dbfc22090a8
Paulina Walaszczyk <paulaszczyk@gmail.com>
2020-10-11 12:23:22 +0200

    used const and no-named functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+{
 function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
@@ -8,7 +9,7 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-function getMoveName(argMoveId){
+const getMoveName = function (argMoveId){ //zrobiłam stałą i funkcję nienazwaną
   if(argMoveId == 1){
     return 'kamień';
   }
@@ -22,14 +23,15 @@ function getMoveName(argMoveId){
     return 'nieznany ruch';
 }
 
-  function displayResult(argComputerMove, argPlayerMove){
+  const displayResult = function(argComputerMove, argPlayerMove){ // utworzyłam stałą i funkcję nienazwaną
     console.log('moves:', argComputerMove, argPlayerMove);
 
   if( (argComputerMove == 'kamień' && argPlayerMove == 'papier') ||( argComputerMove == 'papier' && argPlayerMove == 'nożyce')
-    || ( argComputerMove == 'nożyce' && argPlayerMove == 'kamień')){
+    || (argComputerMove == 'nożyce' && argPlayerMove == 'kamień')){
     printMessage('Ty wygrywasz!');
   }
-  else if (( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') || ( argComputerMove == 'kamień' && argPlayerMove == 'kamień') || ( argComputerMove == 'papier' && argPlayerMove == 'papier')){
+  else if (( argComputerMove == 'nożyce' && argPlayerMove == 'nożyce') || ( argComputerMove == 'kamień' && argPlayerMove == 'kamień') 
+    || (argComputerMove == 'papier' && argPlayerMove == 'papier')){
     printMessage('Remis!');
   }
   else {
@@ -37,24 +39,26 @@ function getMoveName(argMoveId){
   }
 }
 
-function playGame(playerInput){
-  clearMessages();
 
-  let randomNumber = Math.floor(Math.random() * 3 + 1); 
+function playGame(playerInput) {
+  clearMessages();
+  const randomNumber = Math.floor(Math.random() * 3 + 1), 
+  computerMove = getMoveName(randomNumber),  playerMove = getMoveName(playerInput); 
+  // zmieniłam zmienne na stałe i wymieniłam po przecinku. 
+  // Próbowałam funkcję zrobić nienazwaną i zmienić na stałą, ale coś nie wychodziło
 
   console.log('Wylosowana liczba to: ' + randomNumber);
-  
-  let computerMove = getMoveName(randomNumber);
   printMessage('Mój ruch to: ' + computerMove);
-  let playerMove = getMoveName(playerInput);
   printMessage('Twój ruch to: ' + playerMove);
+  
   displayResult(computerMove, playerMove)
 }
 
-var buttonRock = document.getElementById('play-rock');
-var buttonPaper = document.getElementById('play-paper');
-var buttonScissors = document.getElementById('play-scissors');
+let buttonRock = document.getElementById('play-rock'), buttonPaper = document.getElementById('play-paper'), 
+buttonScissors = document.getElementById('play-scissors');
+// zmieniłam var na let i wymieniłam po przecinku. A dlaczego wcześniej było ustawione jako 'var'?
 
 buttonRock.addEventListener('click', function(){playGame(1);});
 buttonPaper.addEventListener('click', function(){playGame(2);});
 buttonScissors.addEventListener('click', function(){playGame(3);});
+}
